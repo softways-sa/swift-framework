3c4c0de0baafbcd79545d4669eccf6ef0d94a27c
Panos Konstantinidis <konstpan@gmail.com>
2017-06-23 13:36:02 +0300

    Add export to excel

--- src/main/java/gr/softways/dev/eshop/emaillists/lists/servlets/doAction.java
+++ src/main/java/gr/softways/dev/eshop/emaillists/lists/servlets/doAction.java
@@ -1,13 +1,14 @@
 package gr.softways.dev.eshop.emaillists.lists.servlets;
 
 import java.io.*;
-import java.util.*;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 import gr.softways.dev.jdbc.*;
 import gr.softways.dev.util.*;
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 public class doAction extends HttpServlet {
 
@@ -27,25 +28,29 @@ public class doAction extends HttpServlet {
   //Process the HTTP Post request
   public void doPost(HttpServletRequest request, HttpServletResponse response) 
               throws ServletException, IOException {
-    request.setCharacterEncoding(_charset);
-    response.setContentType("text/html; charset=" + _charset);
     
-    String action = request.getParameter("action1") == null ? "" : request.getParameter("action1"),
-           databaseId = request.getParameter("databaseId") == null ? "" : request.getParameter("databaseId").trim(),
+    String action = request.getParameter("action1") == null ? "" : request.getParameter("action1");
+    
+    if ("EXCEL".equals(action)) {
+      response.setContentType("application/vnd.ms-excel; charset=" + _charset);
+    }
+    else {
+      request.setCharacterEncoding(_charset);
+      response.setContentType("text/html; charset=" + _charset);
+    }
+    
+    String databaseId = request.getParameter("databaseId") == null ? "" : request.getParameter("databaseId").trim(),
            urlSuccess = request.getParameter("urlSuccess") == null ? "" : request.getParameter("urlSuccess"),
            urlFailure = request.getParameter("urlFailure") == null ? "" : request.getParameter("urlFailure"),
            urlNoAccess = request.getParameter("urlNoAccess") == null ? "" : request.getParameter("urlNoAccess");
 
     int status = Director.STATUS_OK;
 
-    if (databaseId.equals(""))
-      status = Director.STATUS_ERROR;
-    else if (action.equals("INSERT"))
-      status = doInsert(request, databaseId);
-    else if (action.equals("UPDATE"))
-      status = doUpdate(request,databaseId);
-    else if (action.equals("DELETE"))
-      status = doDelete(request,databaseId);
+    if (databaseId.equals("")) status = Director.STATUS_ERROR;
+    else if (action.equals("INSERT")) status = doInsert(request, databaseId);
+    else if (action.equals("UPDATE")) status = doUpdate(request,databaseId);
+    else if (action.equals("DELETE")) status = doDelete(request,databaseId);
+    else if (action.equals("EXCEL")) doExportEXCEL(request,response,databaseId);
 
     if (status < 0) {
       response.sendRedirect(urlNoAccess);
@@ -223,5 +228,97 @@ public class doAction extends HttpServlet {
 
     bean.freeDBConnection(databaseId,database);
     return status;
+  }
+  
+  private DbRet doExportEXCEL(HttpServletRequest request,HttpServletResponse response,String databaseId) {
+    String authUsername = SwissKnife.getSessionAttr(databaseId + ".authUsername",request),
+           authPassword = SwissKnife.getSessionAttr(databaseId + ".authPassword",request);
+
+    int auth = bean.auth(databaseId,authUsername,authPassword,"emailListTab",Director.AUTH_UPDATE);
+
+    DbRet dbRet = new DbRet();
+    
+    if (auth != Director.AUTH_OK) {
+      dbRet.setNoError(0);
+      dbRet.setAuthError(1);
+      dbRet.setAuthErrorCode(auth);
+      return dbRet;
+    }
+    
+    String EMLTCode = SwissKnife.sqlEncode( SwissKnife.grEncode( request.getParameter("EMLTCode") ) );
+    
+    StringBuilder query = new StringBuilder();
+    query.append("SELECT EMLMEmail,EMLTName FROM emailListMember,emailListReg,emailListTab where EMLMCode = EMLRMemberCode AND EMLTCode = EMLRListCode AND EMLMActive = '1'");
+    if (EMLTCode.length() > 0) {
+      query.append(" AND EMLTCode = '").append(EMLTCode).append("'");
+    }
+    query.append(" ORDER BY EMLTName, EMLMEmail");
+    
+    ServletOutputStream out = null;
+    
+    String promptFilename = "newsletter_list_" + EMLTCode;
+    response.addHeader("content-disposition","attachment; filename=" + promptFilename + ".xls");
+
+    WritableWorkbook workbook = null;
+    WritableSheet sheet = null;
+    
+    Director director = Director.getInstance();
+    
+    Database database = director.getDBConnection(databaseId);
+    
+    dbRet = database.beginTransaction(Database.TRANS_ISO_SIMPLE);
+
+    int prevTransIsolation = dbRet.getRetInt();
+    
+    QueryDataSet queryDataSet = null;
+    
+    try {
+      out = response.getOutputStream();
+      
+      workbook = Workbook.createWorkbook(out);
+      sheet = workbook.createSheet("list",0);
+      
+      int excelRow = 0, index = -1;
+      
+      sheet.addCell(new Label(++index, excelRow, "киста"));
+      sheet.addCell(new Label(++index, excelRow, "EMAIL"));
+      
+      queryDataSet = new QueryDataSet();
+      
+      queryDataSet.setQuery(new QueryDescriptor(database,query.toString(),null,true,Load.UNCACHED));
+      queryDataSet.setMetaDataUpdate(MetaDataUpdate.NONE);
+
+      queryDataSet.refresh();
+      
+      while (queryDataSet.inBounds() == true) {
+        excelRow++;
+        
+        index = -1;
+        
+        sheet.addCell(new Label(++index, excelRow, SwissKnife.sqlDecode(queryDataSet.getString("EMLTName"))));
+        sheet.addCell(new Label(++index, excelRow, SwissKnife.sqlDecode(queryDataSet.getString("EMLMEmail"))));
+        
+        queryDataSet.next();
+      }
+      
+      workbook.write();
+    }
+    catch (Exception e) {
+      dbRet.setNoError(0);
+      
+      e.printStackTrace();
+    }
+    finally {
+      try { workbook.close(); } catch (Exception e) { e.printStackTrace(); }
+      try { out.close(); } catch (Exception e) { e.printStackTrace(); }
+      
+      if (queryDataSet != null) try { queryDataSet.close(); } catch (Exception e) { e.printStackTrace(); }
+    }
+    
+    database.commitTransaction(1,prevTransIsolation);
+    
+    director.freeDBConnection(databaseId,database);
+    
+    return new DbRet();
   }
 }
